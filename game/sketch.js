41c9866bd031da6ad125920d81b6e0c7eba011d2
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2020-12-31 18:20:01 +1100

    Update sketch.js

--- game/sketch.js
+++ game/sketch.js
@@ -7,7 +7,7 @@ var sx = 5;
 var sy = 5;
 
 function setup() {
-  b = createArray(sx,sy);
+  b = createArray(sx,sy); 
   for(var i=0;i<b.length;i++){for(var j=0;j<b[0].length;j++){b[i][j]=0}}
   createCanvas(b.length*100+20, b.length[0]*100+20);
   textSize(80);
